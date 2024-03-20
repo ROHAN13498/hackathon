ff21e32ed79dcadd47c6fe2917fe1a3e06daecdd
Anirudh Joshi <vuddawaranirudhjoshi03@gmail.com>
2024-03-20 11:42:32 +0530

    added backend

--- src/components/SExplore.jsx
+++ src/components/SExplore.jsx
@@ -1,128 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SSideBar from './SSideBar';
-import Card from './Card';
+import TCard from './TCard';
 
 const SExplore = () => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [tutors, setTutors] = useState([]);
+    const [cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        // Function to fetch card data
+        const fetchCardData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/students/tutors'); // Assuming your backend API endpoint for fetching courses is /api/courses
+                setCardData(response.data); // Assuming response.data contains the card data
+                setTutors(response.data)
+                console.log(response.data)
+            } catch (error) {
+                console.error('Error fetching card data:', error);
+            }
+        };
+
+        // Call the fetchCardData function
+        fetchCardData();
+    }, []);
 
     const handleSearchChange = (e) => {
         const value = e.target.value;
         setSearchQuery(value);
         if (value.trim() === '') {
             // If search query is empty, reset courses to original data
-            setCourses(cardData); // Assuming cardData contains the original data
+            setTutors(cardData); // Assuming cardData contains the original data
         } else {
             // Filter courses based on search query
-            const filteredCourses = courses.filter((course) =>
-                course.title.toLowerCase().includes(value.toLowerCase())
+            const filteredCourses = cardData.filter((tutor) =>
+                tutor.name.toLowerCase().includes(value.toLowerCase())
             );
-            setCourses(filteredCourses);
+            setTutors(filteredCourses);
         }
     };
 
-    const cardData = [
-        {
-            id: 1,
-            title: 'UI/UX Review Check',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'CODING',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'DOCKER',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'REDIS',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'NEXT',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'RADIX',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'DOCKER',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'Radix',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'Shadcn',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'tailwind',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'UI/UX Review Check',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'UI/UX Review Check',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'UI/UX Review Check',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        {
-            id: 1,
-            title: 'UI/UX Review Check',
-            description: "Because it's about motivating the doers. Because I'm here to follow my dreams and inspire others.",
-            imageUrl: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?size=1200:675',
-            url: 'https://example.com/page1', // URL to explore
-        },
-        // Add more data as needed
-    ];
-
-    const [courses, setCourses] = useState(cardData)
     return (
         <div className="flex">
             <div className="w-64">
@@ -139,14 +56,14 @@ const SExplore = () => {
                     />
                 </div>
                 <div className='flex flex-wrap'>
-                    {courses.map((card) => (
-                        <Card
-                            key={card.id}
-                            title={card.title}
-                            description={card.description}
-                            imageUrl={card.imageUrl}
-                            url={card.url}
-                            btntxt="Enroll"
+                    {tutors.map((tutor) => (
+                        <TCard
+                            key={tutor.userId}
+                            title={tutor.name}
+                            description={tutor.aboutMe}
+                            imageUrl={tutor.imageLink}
+                            url="dummy"
+                            // btntxt="Enroll"
                         />
                     ))}
                 </div>
